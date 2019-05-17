7ecd725145fe01551ecab6ed74eeace0c5ab5297
MaxPostema <max@elynor.com>
2019-05-17 16:25:02 +0200

    Add MainView

---
+++ src/views/MainView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="main-view" class="row">
+    <div class="col-sm-3">
+      sidebar
+    </div>
+    <div class="col-sm-9">
+      content
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'MainView'
+})
+</script>
--- tests/e2e/specs/MainView.js
+++ tests/e2e/specs/MainView.js
@@ -5,10 +5,9 @@ module.exports = {
   'default e2e tests': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js + TypeScript App')
-      .assert.elementCount('img', 1)
+      .waitForElementVisible('#main-view', 5000)
+      .assert.elementPresent('div.row > .col-sm-3')
+      .assert.elementPresent('div.row > .col-sm-9')
       .end()
   }
 }
