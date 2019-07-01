76aae9764f8388cbaaa9c8523a61e3535ed10946
connoratrug <47183404+connoratrug@users.noreply.github.com>
2019-07-01 10:59:25 +0200

    test(store): add unit tests for facet mutations

--- tests/unit/store/mutations.spec.ts
+++ tests/unit/store/mutations.spec.ts
@@ -1,6 +1,73 @@
 import mutations from '@/store/mutations'
+import state from '@/store/state'
 
 describe('mutations', () => {
+  describe('updateGenderFilter', () => {
+    it('replace the filter genders with the selected genders', () => {
+      let baseAppState = Object.assign({}, state)
+      mutations.updateGenderFilter(baseAppState, ['female'])
+      expect(baseAppState.facetFilter.gender).toEqual(['female'])
+    })
+  })
+
+  describe('updateSubcohortfilter', () => {
+    it('replace the filter subcohorts with the selected subcohorts', () => {
+      let baseAppState = Object.assign({}, state)
+      mutations.updateSubcohortfilter(baseAppState, ['ABCD'])
+      expect(baseAppState.facetFilter.subcohort).toEqual(['ABCD'])
+    })
+  })
+
+  describe('updateSelectedAgeAt', () => {
+    it('replace the filter agegroup selections with passed ageAt', () => {
+      let baseAppState = Object.assign({}, state)
+      mutations.updateSelectedAgeAt(baseAppState, {
+        ageGroupAt1A: ['a'],
+        ageGroupAt2A: ['b'],
+        ageGroupAt3A: ['c', 'd']
+      })
+      expect(baseAppState.facetFilter.ageGroupAt1A).toEqual(['a'])
+      expect(baseAppState.facetFilter.ageGroupAt2A).toEqual(['b'])
+      expect(baseAppState.facetFilter.ageGroupAt3A).toEqual(['c', 'd'])
+    })
+  })
+
+  describe('updateYearOfBirthRangefilter', () => {
+    it('replace the filter yob range with the selected yob range', () => {
+      let baseAppState = Object.assign({}, state)
+      mutations.updateYearOfBirthRangefilter(baseAppState, [1960, 2010])
+      expect(baseAppState.facetFilter.yearOfBirthRange).toEqual([1960, 2010])
+    })
+  })
+
+  describe('removeYearOfBirthRangefilter', () => {
+    it('resets the year of birth range filter ', () => {
+      let baseAppState = Object.assign({}, state)
+      mutations.updateYearOfBirthRangefilter(baseAppState, [1960, 2010])
+      expect(baseAppState.facetFilter.yearOfBirthRange).toEqual([1960, 2010])
+      mutations.removeYearOfBirthRangefilter(baseAppState)
+      expect(baseAppState.facetFilter.yearOfBirthRange).toEqual([])
+    })
+  })
+
+  describe('removeAgeAtFilter', () => {
+    it('resets the age at filter ', () => {
+      let baseAppState = Object.assign({}, state)
+      mutations.updateSelectedAgeAt(baseAppState, {
+        ageGroupAt1A: ['a'],
+        ageGroupAt2A: ['b'],
+        ageGroupAt3A: ['c', 'd']
+      })
+      expect(baseAppState.facetFilter.ageGroupAt1A).toEqual(['a'])
+      expect(baseAppState.facetFilter.ageGroupAt2A).toEqual(['b'])
+      expect(baseAppState.facetFilter.ageGroupAt3A).toEqual(['c', 'd'])
+      mutations.removeAgeAtFilter(baseAppState)
+      expect(baseAppState.facetFilter.ageGroupAt1A).toEqual([])
+      expect(baseAppState.facetFilter.ageGroupAt2A).toEqual([])
+      expect(baseAppState.facetFilter.ageGroupAt3A).toEqual([])
+    })
+  })
+
   describe('toggleGridSelection', () => {
     it('selects if none selected', () => {
       const state = {
