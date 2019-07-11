da730e4bd929d84ce8aa0e03bb0e36288d5edbce
connor <c.stroomberg@gmail.com>
2019-07-11 15:41:50 +0200

    task(grid): move store binding out of grid component and into view, pass data via properties, handle user action via events
    - remove unused mutation

--- src/views/GridView.vue
+++ src/views/GridView.vue
@@ -1,15 +1,64 @@
 <template>
   <div id="Grid-view">
-    <Grid />
+    <grid-component
+    v-if="treeSelected != -1"
+    :grid="grid"
+    :gridAssessments="gridAssessments"
+    :gridVariables="gridVariables"
+    :gridSelections="gridSelections"
+    @gridRowToggle="handleGridRowToggle"
+    @gridColumnToggle="handleGridColumnToggle"
+    @gridCellToggle="handleGridCellToggle"
+    @gridAllToggle="handleGridAllToggle"
+    />
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import Grid from '../components/grid/GridComponent.vue'
+import GridComponent from '../components/grid/GridComponent.vue'
+import { mapActions, mapState, mapGetters, mapMutations } from 'vuex'
 
 export default Vue.extend({
-  name: 'ContentView',
-  components: { Grid }
+  name: 'GridView',
+  components: { GridComponent },
+  computed: {
+    ...mapState(['treeSelected', 'gridVariables']),
+    ...mapGetters(['rsql', 'gridAssessments', 'grid', 'gridSelections'])
+  },
+  methods: {
+    ...mapMutations(['toggleGridSelection', 'toggleGridRow', 'toggleGridColumn', 'toggleAll']),
+    ...mapActions(['loadGridVariables', 'loadGridData', 'loadAssessments']),
+    handleGridRowToggle (variableId) {
+      this.toggleGridRow({
+        variableId,
+        gridAssessments: this.gridAssessments
+      })
+    },
+    handleGridColumnToggle (assessmentId) {
+      this.toggleGridColumn({ assessmentId })
+    },
+    handleGridCellToggle (rowIndex, colIndex) {
+      this.toggleGridSelection({
+        variableId: this.gridVariables[rowIndex].id,
+        assessmentId: this.gridAssessments[colIndex].id
+      })
+    },
+    handleGridAllToggle () {
+      this.toggleAll({ gridAssessments: this.gridAssessments })
+    }
+  },
+  watch: {
+    treeSelected: function () {
+      this.loadGridVariables()
+    },
+    rsql: function () {
+      this.loadGridData()
+    }
+  },
+  created: function () {
+    this.loadAssessments()
+    this.loadGridData()
+  }
 })
 </script>
