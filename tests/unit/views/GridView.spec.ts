0c52d6ab59556ea60af9ad352fa40036f4c81ac7
connor <c.stroomberg@gmail.com>
2019-07-12 11:23:10 +0200

    task(grid): add test for rsql watcher

--- tests/unit/views/GridView.spec.ts
+++ tests/unit/views/GridView.spec.ts
@@ -83,4 +83,38 @@ describe('GridView', () => {
       expect(loadGridVariables).toHaveBeenCalled()
     })
   })
+
+  describe('When rsql changes', () => {
+    it('loadGridData action gets called (again)', () => {
+      let rsql = 'abc'
+      wrapper = shallowMount(GridView, { 
+          store,
+          computed: {
+            rsql: {
+                get () {
+                    return rsql
+                },
+                set (value) {
+                    rsql = value
+                }
+            }
+          }
+         })
+
+     /**
+      * This is needed to trigger the watch that is actualy based on a mocked store
+      * 
+      * Best to avoid watching store state 
+      */
+      // @ts-ignore
+     wrapper.setData({ rsql: 'efg'} )
+      // @ts-ignore
+      wrapper.vm._computedWatchers['rsql'].value = 'efg'
+      // @ts-ignore
+      wrapper.vm._watchers.forEach(watcher => {
+        watcher.run()
+       })
+      expect(loadGridDataMock).toHaveBeenCalledTimes(2)
+    })
+  })
 })
