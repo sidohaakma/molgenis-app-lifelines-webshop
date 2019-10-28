992bf68a76ddd9d826ffb411d85d89edd195bf2d
Connor <c.stroomberg@gmail.com>
2019-10-28 17:09:32 +0100

    feat: order from view
    - add molgenis-ui-form for form fields

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainView from './views/MainView.vue'
+import OrderView from './views/OrderView.vue'
 
 Vue.use(Router)
 
@@ -16,9 +17,14 @@ export default new Router({
       component: MainView
     },
     {
-      path: '/:cartId',
+      path: '/shop/:cartId',
       name: 'load',
       component: MainView
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: OrderView
     }
   ]
 })
---
+++ src/views/OrderView.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="container">
+        <h1>Order varibles</h1>
+        <div class="row">
+          <div class="col">
+            <form-component
+            id="order-form"
+            :options="options"
+            :formFields="formFields"
+            :initialFormData="initialFormData"
+            :formState="formState">
+            </form-component>
+            <div>
+              <button class="btn btn-secondary btn-outline" type="button">Cancel</button>
+              <button class="btn btn-primary ml-1" type="button">Submit</button>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { FormComponent } from '@molgenis/molgenis-ui-form'
+
+export default Vue.extend({
+  name: 'OrderView',
+  components: {
+    FormComponent
+  },
+  data () {
+    return {
+      options: {
+        showEyeButton: false,
+        allowAddingOptions: false
+      },
+      formFields: [
+        {
+          type: 'text',
+          id: 'project-number',
+          label: 'Project number',
+          description: 'The OV number.',
+          required: () => true,
+          disabled: false,
+          readOnly: false,
+          visible: () => true,
+          validate: () => true
+        },
+        {
+          type: 'text',
+          id: 'name',
+          label: 'Name',
+          description: 'Optional name',
+          required: () => false,
+          disabled: false,
+          readOnly: false,
+          visible: () => true,
+          validate: () => true
+        },
+        {
+          type: 'file',
+          id: 'application-form ',
+          label: 'Application form ',
+          description: 'Word or text file to describe the request.',
+          required: () => false,
+          disabled: false,
+          readOnly: false,
+          visible: () => true,
+          validate: () => true
+        }
+      ],
+      initialFormData: {},
+      formState: {}
+    }
+  }
+})
+</script>
