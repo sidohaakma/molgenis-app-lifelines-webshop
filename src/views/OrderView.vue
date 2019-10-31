d44d226d2f0e69b6e64186c4c1dc62ac6252bc93
connoratrug <47183404+connoratrug@users.noreply.github.com>
2019-10-31 16:48:40 +0100

    fix: add index.html to router base path
    fix: add index.html to router base path

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -130,7 +130,7 @@ export default Vue.extend({
         this.isSaving = true
         await this.submitOrder({ formData: this.formData, formFields: this.formFields }).catch(() => {
           this.isSaving = false
-          this.setToast({ type: 'warning', message: 'Failed to sumbmit order' })
+          this.setToast({ type: 'warning', message: 'Failed to submit order' })
         })
         this.$router.push('/')
       }
