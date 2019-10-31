a20c01c0cff6415dd3629127a5fde3fedb8cb91a
connor <c.stroomberg@gmail.com>
2019-10-31 16:28:15 +0100

    store auth status in the store

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,15 @@
 import Vue from 'vue'
 import PageComponent from '../node_modules/@molgenis/molgenis-ui-context/src/components/PageComponent.vue'
 import '../node_modules/@molgenis/molgenis-ui-context/public/sticky-footer.css'
+import { mapMutations } from 'vuex'
 
 export default Vue.extend({
   name: 'app',
   components: { PageComponent },
   methods: {
+    ...mapMutations(['setIsSignedIn']),
     handleContextLoaded (context) {
-      console.log(context)
+      this.setIsSignedIn(context.authenticated)
     }
   }
 })
