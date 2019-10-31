35c56cb63a09f04da136ffd140f324af2ecf44b4
connor <c.stroomberg@gmail.com>
2019-10-31 10:53:31 +0100

    fix e2e test setup

--- src/router.ts
+++ src/router.ts
@@ -9,7 +9,7 @@ const packageJson = require('../package.json')
 
 export default new Router({
   mode: 'history',
-  base: process.env.NODE_ENV === 'production' ? packageJson.name : process.env.BASE_URL,
+  base: process.env.NODE_ENV === 'production' ? packageJson.name + '/dist/' : process.env.BASE_URL,
   routes: [
     {
       path: '/',
