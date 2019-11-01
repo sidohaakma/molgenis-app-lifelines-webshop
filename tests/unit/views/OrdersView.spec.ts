ab5ac0d479a35be75a355c7e063848484f3adabb
Fleur Kelpin <f.kelpin@umcg.nl>
2019-11-01 15:36:50 +0100

    test: unit tests for ordersview

--- tests/unit/views/OrdersView.spec.ts
+++ tests/unit/views/OrdersView.spec.ts
@@ -4,11 +4,13 @@ import OrdersView from '@/views/OrdersView.vue'
 import moment from 'moment'
 import Vuex from 'vuex'
 import orders from '../fixtures/orders'
+import Spinner from '@/components/animations/SpinnerAnimation.vue'
+import mutations from '@/store/mutations'
 
 describe('OrdersView.vue', () => {
   const localVue = createLocalVue()
   localVue.filter('moment', function (value: string, format: string) { return moment(value).format(format) })
-  localVue.filter('i18n', (value: string) => value)
+  localVue.filter('i18n', (value: string) => `#${value}#`)
 
   localVue.use(Vuex)
   localVue.use(VueRouter)
@@ -21,17 +23,40 @@ describe('OrdersView.vue', () => {
     let state: any
 
     state = {
-      orders
+      orders: null
     }
-
     store = new Vuex.Store({
       state,
-      actions
+      actions,
+      mutations
     })
   })
 
   it('renders orders view', () => {
     const wrapper = shallowMount(OrdersView, { store, localVue })
     expect(wrapper.find('#orders-view').exists()).toBeTruthy()
+  })
+
+  it('shows title', () => {
+    const wrapper = shallowMount(OrdersView, { store, localVue })
+    expect(wrapper.find('#orders-title').text()).toBe('#lifelines-webshop-orders-title#')
+  })
+
+  it('fetches orders when mounted', () => {
+    const wrapper = shallowMount(OrdersView, { store, localVue })
+    expect(actions.loadOrders).toHaveBeenCalled()
+  })
+
+  it('shows a spinner while loading orders', () => {
+    const wrapper = shallowMount(OrdersView, { store, localVue })
+    expect(wrapper.find(Spinner).isVisible()).toBeTruthy()
+    store.commit('setOrders', orders)
+    expect(wrapper.find(Spinner).exists()).toBeFalsy()
+  })
+
+  it('shows orders table when loaded', () => {
+    const wrapper = shallowMount(OrdersView, { store, localVue })
+    store.commit('setOrders', orders)
+    expect(wrapper.find('table').isVisible()).toBeTruthy()
   })
 })
