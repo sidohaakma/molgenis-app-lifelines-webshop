07efd351590db6ca4efb7b7a76d4606ea7e8beb6
Fleur Kelpin <f.kelpin@umcg.nl>
2019-11-19 00:10:42 +0100

    fix: import umd instead of sources
    Jest acts up when I import store sources from ui-context.
    Switching to the umd fixes it.
    
    I think the difference with the api-client which imports correctly
    when I add it to ignoreTransform regex in jest config
    is that the context library is built as a vue-cli library and does not
    export typings.

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,9 +4,10 @@ import state from '@/store/state'
 import actions from '@/store/actions'
 import getters from '@/store/getters'
 import mutations from '@/store/mutations'
-import context from '@molgenis/molgenis-ui-context/src/store'
-
 import ApplicationState from '@/types/ApplicationState'
+
+// @ts-ignore
+const { store: context } = require('@molgenis/molgenis-ui-context').default
 
 const packageJson = require('../../package.json')
 Vue.use(Vuex)
